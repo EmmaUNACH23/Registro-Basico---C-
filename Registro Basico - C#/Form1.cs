08491e43570d9be8c427d3c938dd642d4c5bd649
Emma <Emma@EMMASENPAI_ASUS>
2024-08-31 22:10:34 -0600

    Agregar archivos de proyecto.

---
+++ Registro Basico - C#/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registro_Basico___C_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nombres = textBox1.Text;
+            string apellidos = textBox2.Text;
+            string edad = textBox3.Text;
+            string estatura = textBox4.Text;
+            string telefono = textBox5.Text;
+
+            string genero = "";
+            if (radioButton1.Checked)
+            {
+                genero = "Hombre";
+            }
+            else if (radioButton2.Checked)
+            {
+                genero = "Mujer";
+            }
+            string datos = $"Nombres: {nombres}\r\nApellidos: {apellidos}\r\nTelefono: {telefono}\r\nEstatura: {estatura}\r\nEdad: {edad}años\r\nGenero: {genero}";
+
+            string rutaArchivos = "C:\\Users\\Emma\\Documents\\Programación Avanzada\\Registro Basico - C#\\Datos_C#.txt";
+            bool archivoExiste = File.Exists(rutaArchivos);
+
+            using (StreamWriter writer = new StreamWriter(rutaArchivos, true))
+            {
+                writer.WriteLine(datos);
+            }
+            MessageBox.Show("Datos guardados: \n\n" + datos, "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+        }
+    }
+}
